388d16d1e24238eaba307463a9a81cb686e773bc
AleksandrStrulkov <astrulkov@yandex.ru>
2023-10-21 18:37:12 +0300

    add employees

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,30 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+EMPLOYEES = "north_data/employees_data.csv"
+CUSTOMERS = "north_data/customers_data.csv"
+ORDERS = "north_data/orders_data.csv"
+
+connect = psycopg2.connect(host="localhost", database="north", user="postgres", password="1247")
+
+try:
+	with connect:
+		with connect.cursor() as cursor:
+			with open(EMPLOYEES, 'r') as csvfile:
+				reader = csv.reader(csvfile)
+				next(reader)
+				for row in reader:
+					cursor.execute(
+							"INSERT INTO employees (employee_id,"
+							"first_name,"
+							"last_name,"
+							"title,"
+							"birth_date,"
+							"notes)"
+							"VALUES (%s, %s, %s, %s, %s, %s)", row)
+
+
+finally:
+	connect.close()
+

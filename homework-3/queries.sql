99519490de5309d5c5db82031a5680397123bc17
AleksandrStrulkov <astrulkov@yandex.ru>
2023-10-28 22:06:31 +0300

    adding code to queries.sql 2

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -13,7 +13,11 @@ AND shippers.company_name='United Package'
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock, contact_name, phone FROM products
+JOIN suppliers USING (supplier_id)
+WHERE products.discontinued <> 1 AND products.units_in_stock < 25
+AND products.category_id IN (SELECT category_id FROM categories WHERE category_name IN ('Dairy Products', 'Condiments'))
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
